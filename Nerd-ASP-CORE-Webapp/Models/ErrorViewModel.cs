369b43ec7ce10a169d514df5b28c5e005e67cf01
DESKTOP-IRRNG7Q\mike <mike@webaphorism.com>
2019-07-25 18:46:10 -0400

    First ASP Core Web app
    Init commit

---
+++ Nerd-ASP-CORE-Webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nerd_ASP_CORE_Webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
